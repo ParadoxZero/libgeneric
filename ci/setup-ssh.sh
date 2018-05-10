f61a1f13948076c97bddc15ef31900768939e56d
Travor Liu <travor_lzh@outlook.com>
2018-05-10 07:46:28 -0400

    Using encrypted key

--- ci/setup-ssh.sh
+++ ci/setup-ssh.sh
@@ -2,12 +2,12 @@
 # setup-ssh.sh: load the SSH key 
 
 set -ev
-declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/travis_temp_ssh_key_XXXX)"
+SSH_FILE=~/.ssh/travis_rsa.pub
 # Decrypt the file containing the private key (put the real name of the variables)
 openssl aes-256-cbc \
-	-K $encrypted_XXXXXXXXXX_key \
-	-iv $encrypted_XXXXXXXXXX_iv \
-	-in ".ci/travis_deploy_key.enc" \
+	-K $encrypted_98cebf81b822_key \
+	-iv $encrypted_98cebf81b822_iv \
+	-in "travis_rsa.pub.enc" \
 	-out "$SSH_FILE" -d
 # Enable SSH authentication
 chmod 600 "$SSH_FILE" \
